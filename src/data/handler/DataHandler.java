c8c5beb215125de41bd008dc221aecc81974e87a
phdljr <phdljr@naver.com>
2021-12-09 02:29:49 +0900

    Test

--- src/data/handler/DataHandler.java
+++ src/data/handler/DataHandler.java
@@ -10,8 +10,8 @@ import java.sql.Statement;
 public class DataHandler {
 	
 	private static DataHandler dh;
-	private String ip = "13.124.82.133";
-	private String port = "54830";
+	private String ip = "13.124.82.133"; //계속 바뀜
+	private String port = "54830"; //계속 바뀜
 	private String db = "userdb";
 	private String id = "root";
 	private String pw = "mysql1234";
@@ -35,7 +35,26 @@ public class DataHandler {
 		return dh;
 	}
 	
-	public static String getScore(String id, String pw) {
+	public String getScore() {
+		String result = "asd";
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from user where user_id = '"+id+"' and user_pw = '"+pw+"'");
+			while(rs.next()) {
+				result += rs.getString("id") + 
+						rs.getString("user_id") + 
+						rs.getString("user_pw") + 
+						rs.getString("user_score_1") + 
+						rs.getString("user_score_2") +
+						rs.getString("user_score_3");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public String login(String id, String pw) {
 		String result = "asd";
 		try {
 			Statement st = con.createStatement();
@@ -54,9 +73,11 @@ public class DataHandler {
 		return result;
 	}
 
+
+	//테스트
 	public static void main(String[] args) {
 		DataHandler dh = DataHandler.getInstance();
-		System.out.println(dh.getScore("admin", "admin"));
-		System.out.println(dh.getScore("test", "test123"));
+		//System.out.println(dh.getScore("admin", "admin"));
+		//System.out.println(dh.getScore("test", "test123"));
 	}
 }
