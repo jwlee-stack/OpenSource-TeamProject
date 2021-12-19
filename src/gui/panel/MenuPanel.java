f696377c3770a10758bf7b0b651ba64fa7aa39be
bedlam1 <90903692+bedlam1@users.noreply.github.com>
2021-12-19 20:21:12 +0900

    Update MenuPanel.java

--- src/gui/panel/MenuPanel.java
+++ src/gui/panel/MenuPanel.java
@@ -3,14 +3,22 @@ package gui.panel;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.SystemColor;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import data.Player;
 import gui.frame.GameFrame;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+import client.Client;
+
+import javax.swing.JTable;
 
 /**
  * 메뉴 Panel. 해당 화면에서 랭크 확인 및 게임 시작을 할 수 있음
@@ -19,19 +27,27 @@ import gui.frame.GameFrame;
  */
 public class MenuPanel extends JPanel{
 	private static final long serialVersionUID = 1L;
-	private JTextField Score;
-	private JTextField MyScore;
-	private JTextField Rnak1_Score;
-	private JTextField Rnak2_Score;
-	private JTextField Rnak3_Score;
-	private JTextField My_Nickname;
-	private JTextField Rank1_Nickname;
-	private JTextField Rank2_Nickname;
-	private JTextField Rank3_Nickname;
+	
+	
+	/**
+	 *   panel
+	 *   label : label[9] - 스코어 컨트롤, label[0] - 나의 등수
+	 *   그외 - 등수(1등, 2등...)
+	 *   text_Nickname : 랭킹 유저들의 닉네임([0] - 나의 닉네임)
+	 *   text_Score : 랭킹 유저들의 점수([0] - 나의 점수)
+	 *   btn : label[9]를 컨트롤할수 있는 버튼
+	 * */
+	JPanel[] panel = new JPanel[2];
+	JLabel[] label= new JLabel[10];
+	JTextField[] text_Nickname = new JTextField[10];
+	JTextField[] text_Score = new JTextField[10];
+	JButton[] btn = new JButton[4];
+	String[] btntext= {"Score1", "Score2", "Score3", "Total"};
 
 	public MenuPanel(GameFrame gf) {
 		setBackground(SystemColor.info);
 		setLayout(null);
+		setSize(800,600);
 		
 		Player player = gf.getClient().getPlayer();
 		
@@ -68,173 +84,111 @@ public class MenuPanel extends JPanel{
 		lbTitle.setBounds(157, 36, 485, 60);
 		add(lbTitle);
 		
-		
-		/**
-		 *   나의 점수
-		 */
-		
-		JPanel My_Info = new JPanel();
-		My_Info.setBounds(410, 170, 370, 90);
-		add(My_Info);
-		My_Info.setLayout(null);
-		
-		JLabel Info_label = new JLabel("");
-		Info_label.setFont(new Font("굴림", Font.PLAIN, 24));
-		Info_label.setHorizontalAlignment(SwingConstants.CENTER);
-		Info_label.setBounds(190, 0, 180, 90);
-		My_Info.add(Info_label);
-		
-		JButton btnNewButton = new JButton("Score1");
-		btnNewButton.setFont(new Font("Franklin Gothic Book", Font.PLAIN, 14));
-		btnNewButton.setBounds(10, 10, 80, 35);
-		btnNewButton.addActionListener((e)->{
-			Info_label.setText("Score1 :");
-		});
-		My_Info.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Score2");
-		btnNewButton_1.setFont(new Font("Franklin Gothic Book", Font.PLAIN, 14));
-		btnNewButton_1.setBounds(100, 10, 80, 35);
-		btnNewButton_1.addActionListener((e)->{
-			Info_label.setText("Score2 :");
-		});
-		My_Info.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Score3");
-		btnNewButton_2.setFont(new Font("Franklin Gothic Book", Font.PLAIN, 14));
-		btnNewButton_2.setBounds(10, 45, 80, 35);
-		btnNewButton_2.addActionListener((e)->{
-			Info_label.setText("Score3 :");
-		});
-		My_Info.add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("Total");
-		btnNewButton_3.setFont(new Font("Franklin Gothic Book", Font.PLAIN, 14));
-		btnNewButton_3.setBounds(100, 45, 80, 35);
-		btnNewButton_3.addActionListener((e)->{
-			Info_label.setText("Total :");
-		});
-		My_Info.add(btnNewButton_3);
+		init();
+		Score();
 		
 		
-		
-		/**
-		 *   나의 랭크와 점수
-		 */
-		JPanel Panel1 = new JPanel();
-		Panel1.setLayout(null);
-		Panel1.setBounds(410, 260, 370, 70);
-		add(Panel1);
-		
-		JLabel MyRank = new JLabel("");
-		MyRank.setFont(new Font("Dialog", Font.PLAIN, 16));
-		MyRank.setForeground(Color.BLACK);
-		MyRank.setHorizontalAlignment(SwingConstants.CENTER);
-		MyRank.setBounds(0, 0, 110, 70);
-		Panel1.add(MyRank);
-		
-		My_Nickname = new JTextField();
-		My_Nickname.setHorizontalAlignment(SwingConstants.CENTER);
-		My_Nickname.setFont(new Font("Dialog", Font.PLAIN, 16));
-		My_Nickname.setEditable(false);
-		My_Nickname.setBounds(110, 0, 130, 70);
-		Panel1.add(My_Nickname);
-		My_Nickname.setColumns(10);
-		
-		MyScore = new JTextField();
-		MyScore.setFont(new Font("Dialog", Font.PLAIN, 16));
-		MyScore.setHorizontalAlignment(SwingConstants.CENTER);
-		MyScore.setEditable(false);
-		MyScore.setBounds(240, 0, 130, 70);
-		Panel1.add(MyScore);
-		MyScore.setColumns(10);
-		
-		/**
-		 *   1,2,3등의 점수
-		 */
-		
-		JPanel Panel2 = new JPanel();
-		Panel2.setLayout(null);
-		Panel2.setBounds(410, 330, 370, 70);
-		add(Panel2);
-		
-		JLabel Rank1 = new JLabel("1등");
-		Rank1.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rank1.setHorizontalAlignment(SwingConstants.CENTER);
-		Rank1.setBounds(0, 0, 110, 70);
-		Panel2.add(Rank1);
-		
-		Rank1_Nickname = new JTextField();
-		Rank1_Nickname.setHorizontalAlignment(SwingConstants.CENTER);
-		Rank1_Nickname.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rank1_Nickname.setEditable(false);
-		Rank1_Nickname.setColumns(10);
-		Rank1_Nickname.setBounds(110, 0, 130, 70);
-		Panel2.add(Rank1_Nickname);
-		
-		Rnak1_Score = new JTextField();
-		Rnak1_Score.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rnak1_Score.setHorizontalAlignment(SwingConstants.CENTER);
-		Rnak1_Score.setEditable(false);
-		Rnak1_Score.setColumns(10);
-		Rnak1_Score.setBounds(240, 0, 130, 70);
-		Panel2.add(Rnak1_Score);
-		
-		
-		JPanel Panel3 = new JPanel();
-		Panel3.setLayout(null);
-		Panel3.setBounds(410, 400, 370, 70);
-		add(Panel3);
-		
-		JLabel Rank2 = new JLabel("2등");
-		Rank2.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rank2.setHorizontalAlignment(SwingConstants.CENTER);
-		Rank2.setBounds(0, 0, 110, 70);
-		Panel3.add(Rank2);
-		
-		Rank2_Nickname = new JTextField();
-		Rank2_Nickname.setHorizontalAlignment(SwingConstants.CENTER);
-		Rank2_Nickname.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rank2_Nickname.setEditable(false);
-		Rank2_Nickname.setColumns(10);
-		Rank2_Nickname.setBounds(110, 0, 130, 70);
-		Panel3.add(Rank2_Nickname);
-		
-		Rnak2_Score = new JTextField();
-		Rnak2_Score.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rnak2_Score.setHorizontalAlignment(SwingConstants.CENTER);
-		Rnak2_Score.setEditable(false);
-		Rnak2_Score.setColumns(10);
-		Rnak2_Score.setBounds(240, 0, 130, 70);
-		Panel3.add(Rnak2_Score);
-		
-		
-		JPanel Panel4 = new JPanel();
-		Panel4.setLayout(null);
-		Panel4.setBounds(410, 470, 370, 70);
-		add(Panel4);
-		
-		JLabel Rank3 = new JLabel("3등");
-		Rank3.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rank3.setHorizontalAlignment(SwingConstants.CENTER);
-		Rank3.setBounds(0, 0, 110, 70);
-		Panel4.add(Rank3);
-		
-		Rank3_Nickname = new JTextField();
-		Rank3_Nickname.setHorizontalAlignment(SwingConstants.CENTER);
-		Rank3_Nickname.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rank3_Nickname.setEditable(false);
-		Rank3_Nickname.setColumns(10);
-		Rank3_Nickname.setBounds(110, 0, 130, 70);
-		Panel4.add(Rank3_Nickname);
-		
-		Rnak3_Score = new JTextField();
-		Rnak3_Score.setFont(new Font("Dialog", Font.PLAIN, 16));
-		Rnak3_Score.setHorizontalAlignment(SwingConstants.CENTER);
-		Rnak3_Score.setEditable(false);
-		Rnak3_Score.setColumns(10);
-		Rnak3_Score.setBounds(240, 0, 130, 70);
-		Panel4.add(Rnak3_Score);
 		
 	}
+	/**
+	 *   점수판 관련 패널, 버튼, 레이블, 텍스트 초기화
+	 * */
+	public void init() {
+		for(int i=0; i<=1; i++) {
+			panel[i] = new JPanel();
+			add(panel[i]);
+			panel[i].setLayout(null);
+		}
+		for(int i=0;i<=3;i++) {
+			btn[i] = new JButton(btntext[i]);
+			btn[i].setVisible(true);
+			panel[0].add(btn[i]);
+		}
+		for(int i=0; i<=9; i++) {
+			label[i] = new JLabel();
+			label[i].setVisible(true);
+		}
+		for(int i=0; i<=9; i++) {
+			text_Nickname[i] = new JTextField();
+			text_Score[i] = new JTextField();
+			text_Nickname[i].setVisible(true);
+			text_Score[i].setVisible(true);
+		}
+	}
+	
+	/**
+	 *   점수판 컨트롤러(버튼을 누르면 해당 게임만의 랭킹스코어를 볼수있다)
+	 * */
+	public void Score(){
+		panel[0].setBounds(410, 170, 370, 90);
+		panel[0].add(label[9]);
+		
+		panel[1].setBounds(410,260,370,270);
+		for(int i=0; i<=8; i++) {
+			panel[1].add(label[i]);
+			panel[1].add(text_Nickname[i]);
+			panel[1].add(text_Score[i]);
+		}
+		
+
+		label[9].setBounds(190, 0, 180, 90);
+		label[9].setFont(new Font("굴림", Font.PLAIN, 24));
+		label[9].setHorizontalAlignment(SwingConstants.CENTER);
+		
+		btn[0].setBounds(10, 10, 80, 35);
+		btn[1].setBounds(100, 10, 80, 35);
+		btn[2].setBounds(10, 45, 80, 35);
+		btn[3].setBounds(100, 45, 80, 35);
+		for(int i=0; i<=3; i++) {
+			btn[i].setFont(new Font("Franklin Gothic Book", Font.PLAIN, 14));
+			
+		}
+		btn[0].addActionListener((e)->{
+			label[9].setText(btntext[0]);
+		});
+		btn[1].addActionListener((e)->{
+			label[9].setText(btntext[1]);
+		});
+		btn[2].addActionListener((e)->{
+			label[9].setText(btntext[2]);
+		});
+		btn[3].addActionListener((e)->{
+			label[9].setText(btntext[3]);
+		});
+		
+		
+		/**
+		 *   점수판(나와 상대유저들의 랭크 및 스코어)
+		 *   label[0]은 따로처리
+		 * */
+		for(int i=0; i<=8; i++) {
+			label[i].setBounds(0,30*i,110,30);
+		}
+		for(int i=1; i<=8; i++) {
+			label[i].setFont(new Font("Dialog", Font.PLAIN, 16));
+			label[i].setHorizontalAlignment(SwingConstants.CENTER);
+			if(i!=0) {
+			label[i].setText(Integer.toString(i)+"등");
+			}
+			else {//label[0](나의 닉네임)일때 
+				
+			}
+		}
+		
+		for(int i=0; i<=8; i++) {
+			text_Nickname[i].setBounds(110, 30*i, 130, 30);
+			text_Score[i].setBounds(240, 30*i, 130, 30);
+			text_Nickname[i].setHorizontalAlignment(SwingConstants.CENTER);
+			text_Score[i].setHorizontalAlignment(SwingConstants.CENTER);
+			text_Nickname[i].setFont(new Font("Dialog", Font.PLAIN, 16));
+			text_Score[i].setFont(new Font("Dialog", Font.PLAIN, 16));
+			text_Nickname[i].setEditable(false);
+			text_Score[i].setEditable(false);
+		}
+		
+		
+		
+	}
+	
+	
 }
